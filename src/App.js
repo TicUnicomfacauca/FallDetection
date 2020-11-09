f9a1f4a366947a3774e5eff7d7b3f7de55f02553
Helder Castrillón <heldersoft@gmail.com>
2020-11-09 13:16:27 -0500

    add About

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import Alert from './Alert.svg';
 import './App.css';
 import { preguntarPermisos,inicializarFirebase,getFallDetection} from "./Components/firebaseConfig";
-
+import About from './Components/About'
+import { Button } from '@material-ui/core';
 const SetAlert=(props)=>{
   if(props.fallstatus!=undefined){
     if(props.fallstatus.cama_hospitalaria.estado==="Fall"){
@@ -25,13 +26,16 @@ function App() {
     preguntarPermisos();    
     getFallDetection(setFallStatus);
 }, []);
+const [about,SetAbout]=useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <h2>
             Fall detecction App
         </h2>
-        <SetAlert fallstatus={fallstatus} />
+        {about===true?<About/>:<SetAlert fallstatus={fallstatus} />}
+        <br/>
+        {about===true?<Button variant="contained" color="primary" onClick={()=>SetAbout(false)}>Main</Button>:<Button variant="contained" color="primary" onClick={()=>SetAbout(true)}>About project</Button>}
         </header>
     </div>
   );
--- src/Components/about.js
+++ src/Components/about.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-const About=()=>{
-    return(
-        <div>Proyecto Creado por</div>
-    )
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+});
+
+function About(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="h5" component="h3">
+        web application to alert about the fall of people from beds or stretchers.
+        </Typography>
+        <Typography component="h6">
+        Project funded by the Comfacauca University Corporation, UEES 2019 call.
+            <div style={{textAlign:'center'}}>
+                  <strong>Investigadores Principales:</strong><br/>
+                  <a href="mailto:cristianchilito@unicomfacauca.edu.co">Jóven Investigador Ing. Cristian Duban Chilito</a> <br/>       
+                  <a href="mailto:jfmunoz@unicomfacauca.edu.co">Mg. Julian Fernando Muñoz</a>    <br/>    
+                  <a href="mailto:lerazo@unicomfacauca.edu.co">Mg. Leydi Rocio Erazo</a>      <br/>  
+                  <a href="mailto:hcastrillon@unicomfacauca.edu.co">PhD. Helder Yesid Castrillón</a> <br/>  
+                  <p>Corporación Universitaria Comfacauca - 2020 </p>          
+                  <p><a href="https://github.com/TicUnicomfacauca/FallDetection" target="_blank"> Código fuente</a></p>
+                  <p>Licencia MIT</p>
+                  <strong>Research group:</strong>
+                  <br></br>
+                  <a href="http://www.unicomfacauca.edu.co/index.php/investigacion/grupos-de-investigacion/grupo-tic-unicomfacauca" target="_blank"><img src="http://www.unicomfacauca.edu.co/cache/mod_roksprocket/01fd3c28e20a9591fc5ae0e7ac32c16e_200_200.png"/></a>
+                </div>
+          </Typography>
+      </Paper>
+    </div>
+  );
 }
 
-export default  About;
+About.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(About);
